3622e6f5343f95e79d6bb2665f2b07f26b4c9e42
Breno25 <brenomatos16@gmail.com>
2022-07-16 03:09:59 -0300

    desafio dio

--- desafios/desafio1.js
+++ desafios/desafio1.js
@@ -1,5 +1,32 @@
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
 
-let employee = {};
-employee.code = 10;
-employee.name = "John";
+// Resposta 1
+
+const funcionario = {
+    codigo: 10,
+    nome: 'João',
+};
+
+// Resposta 2
+
+const funcionario2 : (codigo: number, nome: String) = {
+    codigo: 10,
+    nome: 'João'
+};
+
+// Resposta 3 e 4
+
+interface Funcionario {
+
+    codigo: number,
+    nome: string
+};
+
+const funcionarioObj = {} as Funcionario;
+funcionarioObj.codigo = 10;
+funcionarioObj.nome = 'João';
+
+const funcionarioObj2: Funcionario = {
+    codigo: 10,
+    nome: 'João'
+};
+
--- desafios/desafio3.js
+++ desafios/desafio3.js
@@ -1,27 +1,41 @@
-// O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
-
 let botaoAtualizar = document.getElementById('atualizar-saldo');
 let botaoLimpar = document.getElementById('limpar-saldo');
-let soma = document.getElementById('soma');
+let soma = document.getElementById('soma')! as HTMLInputElement;
 let campoSaldo = document.getElementById('campo-saldo');
 
-campoSaldo.innerHTML = 0
+let saldoTotal = 0;
 
-function somarAoSaldo(soma) {
-    campoSaldo.innerHTML += soma;
+function somarAoSaldo(soma: number) {
+    if(campoSaldo) {
+        saldoTotal += soma;
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
 }
 
 function limparSaldo() {
-    campoSaldo.innerHTML = '';
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
 }
 
-botaoAtualizar.addEventListener('click', function () {
-    somarAoSaldo(soma.value);
-});
+if(botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', () => {
+        somarAoSaldo(Number(soma.value));
+    });
+}
 
-botaoLimpar.addEventListener('click', function () {
-    limparSaldo();
-});
+if (botaoLimpar) {
+    botaoLimpar.addEventListener('click', () => {
+        limparSaldo();
+    });
+}
+
+
+function limparCampoSoma() {
+    soma.value = "";
+}
 
 /**
     <h4>Valor a ser adicionado: <input id="soma"> </h4>
